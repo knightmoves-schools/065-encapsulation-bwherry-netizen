c89bb382f9bd0bcc8a345f0ca21d2f971b76f1b4
Brendon W. <bwherry@midland.k12.ia.us>
2025-11-25 10:38:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-
+let calculateArea = length * height;
 let length = 5;
 let height = 10;
 
-function calculateArea(length, height){
-    return length * height;
+
+
+
+class Rectangle {
+    constructor(length, height){
+        this.length = length;
+        this.height = height;
+    }
+
+    calculateArea() {
+    return this.length * this.height;
+    }  
 }
+
+
